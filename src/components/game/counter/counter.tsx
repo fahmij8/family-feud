5711deb735a95ebc75c6cc1851baca1f651bbbef
fahmij8 <fahmijabbar12@gmail.com>
2023-12-11 20:53:37 +0700

    feat: change damping & stiffness counter

--- src/components/game/counter/counter.tsx
+++ src/components/game/counter/counter.tsx
@@ -1,35 +1,37 @@
-import { useEffect, useRef } from 'react';
+import { memo, useEffect, useRef } from 'react';
 import { useMotionValue, useSpring } from 'framer-motion';
 
-export const Counter = ({
-  value,
-  direction = 'up',
-}: {
-  value: number;
-  direction?: 'up' | 'down';
-}) => {
-  const ref = useRef<HTMLSpanElement>(null);
-  const motionValue = useMotionValue(direction === 'down' ? value : 0);
-  const springValue = useSpring(motionValue, {
-    damping: 100,
-    stiffness: 100,
-  });
+export const Counter = memo(
+  ({
+    value,
+    direction = 'up',
+  }: {
+    value: number;
+    direction?: 'up' | 'down';
+  }) => {
+    const ref = useRef<HTMLSpanElement>(null);
+    const motionValue = useMotionValue(direction === 'down' ? value : 0);
+    const springValue = useSpring(motionValue, {
+      damping: 50,
+      stiffness: 200,
+    });
 
-  useEffect(() => {
-    motionValue.set(direction === 'down' ? 0 : value);
-  }, [motionValue, value, direction]);
+    useEffect(() => {
+      motionValue.set(direction === 'down' ? 0 : value);
+    }, [motionValue, value, direction]);
 
-  useEffect(
-    () =>
-      springValue.on('change', latest => {
-        if (ref.current) {
-          ref.current.textContent = Intl.NumberFormat('en-US').format(
-            latest.toFixed(0),
-          );
-        }
-      }),
-    [springValue],
-  );
+    useEffect(
+      () =>
+        springValue.on('change', latest => {
+          if (ref.current) {
+            ref.current.textContent = Intl.NumberFormat('en-US').format(
+              latest.toFixed(0),
+            );
+          }
+        }),
+      [springValue],
+    );
 
-  return <span ref={ref}>0</span>;
-};
+    return <span ref={ref}>0</span>;
+  },
+);
